f9f4cda4f93d6b6ebc98dcb65330a004a5d59abe
Robel <robel.tirfe@gmail.com>
2019-01-22 19:08:43 -0500

    API calls added.

--- app/src/main/java/com/example/robel/testideavim/MainActivity.java
+++ app/src/main/java/com/example/robel/testideavim/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.robel.testideavim;
 
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
+//TODO test hooking getSystemService(Context.CONNECTIVITY_SERVICE);
+//android.content.ContextWrapper.getApplicationContext();
+//"com.android.inputmethod.keyboard"
 
 public class MainActivity extends AppCompatActivity {
     private int count = 0;
     Button crackButtion;
+    StringBuilder myNetworkStatus = new StringBuilder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myNetworkStatus.append("Network hasConnection: ");
+        myNetworkStatus.append(this.hasInternetConnection().toString());
+        Toast.makeText(getApplicationContext(),this.getActiveNetwork(),Toast.LENGTH_LONG).show();
 
         crackButtion= (Button)findViewById(R.id.button2);
         crackButtion.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +44,33 @@ public class MainActivity extends AppCompatActivity {
        else {
             Toast.makeText(getApplicationContext(),"You can't crack it",Toast.LENGTH_LONG).show();
        }
+    }
+
+    private Boolean hasInternetConnection(){
+        //ConnectivityManager =>Class that answers queries about the state of network connectivity.
+        //It also notifies applications when network connectivity changes.
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return new Boolean(isConnected);
+    }
+
+    private String getActiveNetwork(){
+        if (hasInternetConnection().toString().equals("false") )
+                return "No internet connection at this moment.";
+
+        else {
+            Context context = getApplicationContext();
+            ConnectivityManager cm ;
+            cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo activeNetwork ;
+            activeNetwork = cm.getActiveNetworkInfo();
+            String type = activeNetwork.getTypeName();
+            return type;
+        }
     }
 }
